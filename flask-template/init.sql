a6d35485fafd0eda906aaab5e686aad4819d37e0
LianGee <bo_bo0425@126.com>
2020-04-09 23:42:44 +0800

    Update init.sql

---
+++ flask-template/init.sql
@@ -0,0 +1,18 @@
+create table if not exists galio.template
+(
+  id         bigint(11) auto_increment comment '主键'
+    primary key,
+  created_at bigint(11)             null comment '创建时间',
+  updated_at bigint(11)             null comment '更新时间',
+  is_delete  tinyint(3)  default 0  null comment '是否删除'
+)
+  comment '模板' charset = utf8mb4;
+
+create index idx_created_at
+  on galio.template (created_at);
+
+create index idx_is_delete
+  on galio.template (is_delete);
+
+create index idx_updated_at
+  on galio.template (updated_at);
